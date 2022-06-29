d34554a124b3da5f4eb6a78697050c29a3c62787
توسعه دهنده زیر ساخت نوید لطفیان <N.Lotfian@exir.rayaneh>
2022-06-29 11:03:07 +0430

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace New_folder__4_.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
